4fc919db753bdc2b99a371787554911d8ce4594f
Anthony4m <amoahanthony41@gmail.com>
2024-05-15 23:23:29 +0000

    refactored

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,22 +1,25 @@
 import {useQuery, useQueryClient} from "react-query";
-import IssueItem from "./IssuesItem";
+import {IssueItem} from "./IssuesItem";
 import {useState} from "react";
 import fetchWithErrors from "../helpers/fetchWithErrors";
 import Loader from "./Loader";
 
 
-export default function IssuesList({labels,status}) {
+export default function IssuesList({labels,status,pageNum,setPageNum}) {
     const queryClient = useQueryClient();
-    const issueQuery = useQuery(['issues',{labels},{status}],
+    const issuesQuery = useQuery(['issues',{labels},{status},{pageNum}],
         async ({signal}) => {
             const statusString = status ? `&status=${status}` : "";
             const labelString = labels.map(label=>`labels[]=${label}`).join('&')
-            const results = await fetchWithErrors(`/api/issues?${labelString}${statusString}`,{signal})
+            const paginationString = pageNum ? `&page=${pageNum}`:'';
+            const results = await fetchWithErrors(`/api/issues?${labelString}${statusString}${paginationString}`,{signal})
 
             results.forEach((issue)=>{
                 queryClient.setQueryData(["issues",issue.number.toString()],issue);
             })
             return results;
+        },{
+        keepPreviousData: true,
         });
     const [searchValue,setSearchValue] = useState("")
 
@@ -40,45 +43,89 @@ export default function IssuesList({labels,status}) {
                     }}
                 }/>
             </form>
-            <h2>Issues List {issueQuery.isFetching ? <Loader/> : null}</h2>
-            { issueQuery.isLoading ? (<p>Loading....</p>)
-                : issueQuery.isError ? <p>{issueQuery.error.message}</p> :searchQuery.fetchStatus === "idle" && searchQuery.isLoading === true ?
-                    (<ul className="issues-list">
-                        {issueQuery.data.map((issue)=>(
-                            <IssueItem key={issue.id}
-                                       title = {issue.title}
-                                       number={issue.number}
-                                       assignee={issue.assignee}
-                                       commentCount={issue.comments.length}
-                                       createdBy={issue.createdBy}
-                                       createdDate={issue.createdDate}
-                                       labels={issue.labels}
-                                       status={issue.status}
+            <h2>
+                Issues List {issuesQuery.fetchStatus === "fetching" ? <Loader /> : null}
+            </h2>
+            {issuesQuery.isLoading ? (
+                <p>Loading...</p>
+            ) : issuesQuery.isError ? (
+                <p>{issuesQuery.error.message}</p>
+            ) : searchQuery.fetchStatus === "idle" &&
+            searchQuery.isLoading === true ? (
+                <>
+                    <ul className="issues-list">
+                        {issuesQuery.data.map((issue) => (
+                            <IssueItem
+                                key={issue.id}
+                                title={issue.title}
+                                number={issue.number}
+                                assignee={issue.assignee}
+                                commentCount={issue.comments.length}
+                                createdBy={issue.createdBy}
+                                createdDate={issue.createdDate}
+                                labels={issue.labels}
+                                status={issue.status}
                             />
                         ))}
                     </ul>
-                    ) : <>
-                        <h2>Search Results</h2>
-                        {searchQuery.isLoading ? <p>Loading...</p> :
-                            <>
-                                <p>{searchQuery.data.count} Results</p>
-                                <ul className="issues-list">
-                                    {searchQuery.data.items.map((issue)=>(
-                                        <IssueItem key={issue.id}
-                                                   title = {issue.title}
-                                                   number={issue.number}
-                                                   assignee={issue.assignee}
-                                                   commentCount={issue.comments.length}
-                                                   createdBy={issue.createdBy}
-                                                   createdDate={issue.createdDate}
-                                                   labels={issue.labels}
-                                                   status={issue.status}
-                                        />))}
-                                </ul>
-                            </>
-                        }
-                    </>
-            }
+                    <div className="pagination">
+                        <button
+                            onClick={() => {
+                                if (pageNum - 1 > 0) {
+                                    setPageNum(pageNum - 1);
+                                }
+                            }}
+                            disabled={pageNum === 1}
+                        >
+                            Previous
+                        </button>
+                        <p>
+                            Page {pageNum} {issuesQuery.isFetching ? "..." : ""}
+                        </p>
+                        <button
+                            disabled={
+                                issuesQuery.data?.length === 0 || issuesQuery.isPreviousData
+                            }
+                            onClick={() => {
+                                if (
+                                    issuesQuery.data?.length !== 0 &&
+                                    !issuesQuery.isPreviousData
+                                ) {
+                                    setPageNum(pageNum + 1);
+                                }
+                            }}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <h2>Search Results</h2>
+                    {searchQuery.isLoading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <>
+                            <p>{searchQuery.data.count} Results</p>
+                            <ul className="issues-list">
+                                {searchQuery.data.items.map((issue) => (
+                                    <IssueItem
+                                        key={issue.id}
+                                        title={issue.title}
+                                        number={issue.number}
+                                        assignee={issue.assignee}
+                                        commentCount={issue.comments.length}
+                                        createdBy={issue.createdBy}
+                                        createdDate={issue.createdDate}
+                                        labels={issue.labels}
+                                        status={issue.status}
+                                    />
+                                ))}
+                            </ul>
+                        </>
+                    )}
+                </>
+            )}
         </div>
     );
 }
--- src/components/StatusSelect.jsx
+++ src/components/StatusSelect.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
-
 const possibleStatus = [
-    {id: "backlog",label:"Backlog"},
-    {id: "todo",label:"Todo"},
-    {id: "InProgress",label:"In Progress"},
-    {id: "done",label:"Done"},
-    {id: "cancelled",label:"Cancelled"},
-]
-
-const StatusSelect = ({value,onChange,noEmptyOption=false}) =>{
-
-   return (
-       <select className="status-select" value={value} onChange={onChange}>
-           {noEmptyOption ? null : <option value="">Select A value to filter</option>}
-           {possibleStatus.map(status=>(
-               <option key={status.id} value={status.id}>{status.label}</option>
-           ))}
-       </select>
-   )
-};
-
-export default StatusSelect;
+    { id: "backlog", label: "Backlog" },
+    { id: "todo", label: "To-do" },
+    { id: "inProgress", label: "In Progress" },
+    { id: "done", label: "Done" },
+    { id: "cancelled", label: "Cancelled" },
+];
+export function StatusSelect({ value, onChange, noEmptyOption = false }) {
+    return (
+        <select value={value} onChange={onChange} className="status-select">
+            {noEmptyOption ? null : (
+                <option value="">Select a status to filter</option>
+            )}
+            {possibleStatus.map((status) => (
+                <option value={status.id} key={status.id}>
+                    {status.label}
+                </option>
+            ))}
+        </select>
+    );
+}
